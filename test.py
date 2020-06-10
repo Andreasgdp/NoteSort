e90227bf0609707fc6444f5df0b625b332da9792
Andreas <Andreasgdp@gmail.com>
2020-06-10 10:19:57 +0200

    All slides are made, but can be polished

--- test.py
+++ test.py
@@ -1,13 +1,15 @@
-from sklearn.feature_extraction.text import CountVectorizer
+test = [1, 2, 3, 4]
 
-sentences = ['I dansk kan man skrive med bogstager', 'I matematik kan man regne med tal']
+for index, element in enumerate(test):
+	if element == 2:
+		element = 0
 
-vectorizer = CountVectorizer(min_df=0, lowercase=False)
-vectorizer.fit(sentences)
+print(test)
+# [1, 2, 3, 4]
 
-print(vectorizer.vocabulary_)
-# {'dansk': 1, 'kan': 2, 'man': 3, 'skrive': 7, 'med': 5, '
-#  bogstager': 0, 'matematik': 4, 'regne': 6, 'tal': 8}
-print(vectorizer.transform(sentences).toarray())
-# [[1 1 1 1 0 1 0 1 0]
-#  [0 0 1 1 1 1 1 0 1]]
+for index, element in enumerate(test):
+	if element == 2:
+		test[index] = 0
+
+print(test)
+# [1, 0, 3, 4]
